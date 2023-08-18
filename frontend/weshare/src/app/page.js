b244aa58ae9b42cae57c648e9cf723a6e56c707f
joe91330 <130330793+joe91330@users.noreply.github.com>
2023-08-18 10:24:43 +0800

    fix: test

--- frontend/weshare/src/app/page.js
+++ frontend/weshare/src/app/page.js
@@ -1,12 +1,12 @@
 // import Image from "next/image";
 // import styles from "./page.module.css";
 
-import Profile from "../../Components/profile";
+import LogInOut from "../../Components/LogInOut";
 
 export default function Home() {
   return (
     <div>
-      <Profile />
+      <LogInOut />
     </div>
   );
 }
