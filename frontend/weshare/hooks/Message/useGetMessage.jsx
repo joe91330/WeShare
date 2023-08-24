bf8c109cee988a3b135023d6e2f8403bee6b3245
justinli <kobs666666@gmail.com>
2023-08-24 15:44:50 +0800

    feat: fix/https

--- frontend/weshare/hooks/Message/useGetMessage.jsx
+++ frontend/weshare/hooks/Message/useGetMessage.jsx
@@ -49,7 +49,7 @@ const useGetMessages = (userid) => {
     })();
 
     // 建立 socket 連接
-    const newSocket = io("http://13.238.130.147/", {
+    const newSocket = io("https://13.238.130.147/", {
       extraHeaders: {
         Authorization: `Bearer ${accessToken}`,
       },
