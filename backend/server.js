0d56a77087f35acda29bc01edb6549b7ddbec262
Distant22 <sindistant@gmail.com>
2023-08-24 12:46:44 +0000

    fix(server):make cors available on all sites

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const server = http.createServer(app); // Create an HTTP server
 
 const io = require("socket.io")(server, {
    cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
   },
 });
 
