f43032af1d56d50a91ee00476a6d1fc2687f0df2
joe91330 <130330793+joe91330@users.noreply.github.com>
2023-08-24 13:27:32 +0800

    feat: page layout completed

--- frontend/weshare/src/app/ordermanage/[userId]/page.js
+++ frontend/weshare/src/app/ordermanage/[userId]/page.js
@@ -3,21 +3,18 @@
 
 "use client";
 
-import { parseCookies } from "nookies";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import Navbar from "../../../../Components/navbar";
 import Ordermanage from "../../../../Components/ordermanage";
+import styles from "./page.module.scss"
 
-// import styles from "../../../../styles/chatPage.module.scss";
 
 
 export default function Chats({ params }) {
   const { chatId } = params;
   console.log("hXXi", chatId);
-  const cookies = parseCookies();
-  const myid = cookies.userId;
   return (
-    <div>
+    <div className={styles.layout}>
       <Navbar />
       <Ordermanage />
     </div>
